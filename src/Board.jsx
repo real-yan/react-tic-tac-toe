a36e342b253b78bff50f26f52ab5f5d868684805
Yan Real <real.yan.yr@gmail.com>
2021-07-15 07:23:01 -0300

    Finishing main flow of actions of the game

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,35 +1,24 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Square from './Square'
 import './Board.css'
 
 function Board(props) {
-
-    const [values, setValues] = useState(Array(9).fill(null));
-
-    const handleClick = (i) => {
-        let newArr = values.slice();
-        newArr[i] = 'X';
-
-        setValues(newArr);
-    }
-
     return (
         <div>
-            <div className="board-title">Next player: </div>
             <div className="board-row">
-                <Square value={values[0]} onClick={() => handleClick(0)} />
-                <Square value={values[1]} onClick={() => handleClick(1)} />
-                <Square value={values[2]} onClick={() => handleClick(2)} />
+                <Square value={props.values[0]} onClick={() => props.handleClick(0)} />
+                <Square value={props.values[1]} onClick={() => props.handleClick(1)} />
+                <Square value={props.values[2]} onClick={() => props.handleClick(2)} />
             </div> 
             <div className="board-row">
-                <Square value={values[3]} onClick={() => handleClick(3)} />
-                <Square value={values[4]} onClick={() => handleClick(4)} />
-                <Square value={values[5]} onClick={() => handleClick(5)} />
+                <Square value={props.values[3]} onClick={() => props.handleClick(3)} />
+                <Square value={props.values[4]} onClick={() => props.handleClick(4)} />
+                <Square value={props.values[5]} onClick={() => props.handleClick(5)} />
             </div>  
             <div className="board-row">
-                <Square value={values[6]} onClick={() => handleClick(6)} />
-                <Square value={values[7]} onClick={() => handleClick(7)} />
-                <Square value={values[8]} onClick={() => handleClick(8)} />
+                <Square value={props.values[6]} onClick={() => props.handleClick(6)} />
+                <Square value={props.values[7]} onClick={() => props.handleClick(7)} />
+                <Square value={props.values[8]} onClick={() => props.handleClick(8)} />
             </div>   
         </div>  
     )
--- src/Game.jsx
+++ src/Game.jsx
@@ -1,13 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Board from './Board'
 import './Game.css'
 
-function Game(props) {
+function Game() {
+    const [values, setValues] = useState(Array(9).fill(null));
+    const [isXNext, setIsXNext] = useState(true);
+    const [history, setHistory] = useState([{values}]);
+    const [stepNumber, setStepNumber] = useState(0);
+
+    const winner = calculateWinner(history[stepNumber].values);
+    
+    const handleClick = (i) => {
+        
+        const currentHistory = history.slice(0, stepNumber + 1)
+        const currentValue = currentHistory[currentHistory.length - 1].values.slice() 
+
+        if(winner || !!currentValue[i]) {
+            return;
+        }
+        
+        currentValue[i] = isXNext ? 'X' : 'O';
+
+        setValues(currentValue);
+        setIsXNext(!isXNext);
+        setHistory(currentHistory.concat([{values: currentValue}]));
+        setStepNumber(currentHistory.length);
+    }    
+
+    const jumpTo = (step) => {
+        setStepNumber(step);
+        setIsXNext(step % 2 === 0);
+    }
+
     return (
         <div className="game-wrapper">
-            <Board />
+            <div className="game-board">
+                <Board values={history[stepNumber].values} handleClick={handleClick} />
+            </div>
+            
+            <div className="game-history">
+                <div>
+                    { winner ? `Winner: ${winner}` : `Next player: ${(isXNext ? 'X' : 'O')}` }
+                </div>
+                
+                <ol>
+                    {history.map((step, move) => {
+                        return (
+                            <li key={move}>
+                                <button onClick={() => jumpTo(move)}>
+                                    {!move ? "Go to start" : `Go to move #${move} ()`}
+                                </button>
+                            </li>
+                        )
+                    })}
+                </ol>
+            </div>
         </div>
     )
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+        }
+    }
+    return null;
 }
 
 export default Game
